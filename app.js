b4d0f663b1dd997a9b2bd33931d66b8899a6f727
Wesley Souza <wesleyondo@gmail.com>
2024-11-08 20:45:42 -0300

    atualizando o código da aula anterior

--- app.js
+++ app.js
@@ -7,20 +7,19 @@
 let chave = false // o interruptor inicia desligada
 let lampada = true // a lâmpada está ok
 
-// Pré carregamento do arquivo de áudio 
-let som = new Audio("sound/breaking-glass.mp3") // quando é APENAS um som pode colocar ele  dentro do "("sound/breaking-glass.mp3")"
-
+// Pré carregamento de arquivo de áudio
+let som = new Audio("sound/breaking-glass.mp3")
 
 function quebrar() {
-    if (lampada === true){
+    if (lampada === true) {
         document.getElementById('lamp').src = "img/broken.jpg"
-    //reproduzindo um arquivo de áudio no JS
-    //Passo 1: copiar o arquivo de áudio para o projeto
-    //Passo 2: usar a classe Audio (biblioteca interna do JS)
-    //Passo 3: Pré carregar o arquivo de áudio para sincronizar com a troca de imagem (Experiência do usuario)
-    som.play()
-    // apoio a lógica para oJS identificar a lâmpada quebrada
-    lampada = false
+        //reproduzindo um arquivo de áudio no JS
+        //Passo 1: copiar o arquivo de áudio para o projeto
+        //Passo 2: usar a classe Audio (biblioteca interna do JS)
+        //Passo 3: pré carregar o arquivo de áudio para sincronizar com a troca de imagem (Experêmcia do usúrio)
+        som.play()
+        // apoio a lógica para oJS identificar a lâmpada quebrada
+        lampada = false
     }
 }
 
@@ -43,3 +42,53 @@ function onoff() {
 
     }
 }
+
+//Estudo de eventos relacionados a click do mouse (pressionado ou não pressionado) e telas touch
+//Passo 1: Capturar os elementos do html (DOM)
+const botao = document.getElementById('button')
+const lampadaImg = document.getElementById('lamp')
+
+//Passo 2: Manipular o evento mouse pressionado
+//addEventListener ("esculta um evento em tempo real")
+//mousedown (mouse pressionado constantimente)
+//mouseup (soltar o botão do mouse)
+//touchstart (tocar na tela e manter)
+//touchend (deixar de pressionar a tela touch)
+
+//pressionar o botão do mouse e manter
+botao.addEventListener('mousedown', (event) => {
+    event.preventDefault() //ignorar o comportamento padrão
+    //console.log("botão do mouse pressionado")
+    //se a lâmpada estiver intacta e o interruptor principal estiver desligado
+    if (lampada === true && chave === false){
+        lampadaImg.src = "img/on.jpg"
+    }
+})
+
+//soltar o botão do mouse
+botao.addEventListener('mouseup', (event) => {
+    event.preventDefault()
+    //console.log("botão do mouse solto")
+    if (lampada === true && chave === false){
+        lampadaImg.src = "img/off.jpg"
+    }
+
+})
+
+//pressionar a tela toch e manter
+botao.addEventListener('touchstart', (event) => {
+    event.preventDefault()
+    //console.log("tela pressionada")
+    if (lampada === true && chave === false){
+        lampadaImg.src = "img/on.jpg"
+    }
+})
+
+//deixar de pressionar a tela touch
+botao.addEventListener('touchend', (event) => {
+    event.preventDefault()  
+    //console.log("a tela não está sendo pressionada")
+    if (lampada === true && chave === false){
+        lampadaImg.src = "img/off.jpg"
+    }
+})
