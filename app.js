63fa545b5de7f315663f16c74c74d8518697548a
Wesley Souza <wesleyondo@gmail.com>
2024-11-07 22:18:11 -0300

    lampada

--- app.js
+++ app.js
@@ -3,19 +3,39 @@
  * @authorWesley Souza
  */
 
+// variaveis de apoio
+
+let chave = false //o interruptor inicia desligado
+let lampada = true  // a lampada OK (não esta quebrada)
+
 function quebrar() {
-   document.getElementById('lamp').src="img/broken.jpg"
+    if(lampada === true){
+    document.getElementById('lamp').src = "img/broken.jpg"
 
     // reproduzindo um arquivo de audio no JS
     // Passo 1: copiar o arquivo de audio para o projeto
     // Passo 2: usar a classe Audio(biblioteca nterna JS)
     let som = new Audio()
     som.play()
-    som.src= "sound/glassbreaking.wav"
-  
+    som.src = "sound/glassbreaking.wav"
+    //apoio a lógica para o JS identificar a lâmpada quebrada
+    lampada = false
+} 
+
 }
 
-function onoff() {
-    document.getElementById('interruptor').src="img/swon.png"
-    
+function onoff() { //inicio
+    if (chave === false && lampada === true) {
+        // ligar a chave 
+        document.getElementById('interruptor').src = "img/swon.png"
+        chave = true // O JS agora sabe que a chave está ligada
+        //acender a lâmpada
+        document.getElementById('lamp').src = "img/on.jpg"
+
+    } else if(lampada === true) {
+        document.getElementById('interruptor').src = "img/swoff.png"
+        chave = false// O JS agora sabe que a chave está desligado
+        //desligar a lampada
+        document.getElementById('lamp').src = "img/off.jpg"
+    }
 }
